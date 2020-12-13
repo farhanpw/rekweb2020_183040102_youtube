4716e9a9d001401ad06e0f6665efb8a681190d7c
farhanpw <farhan.fpw@gmail.com>
2020-12-13 15:48:50 +0700

    rest_api#6

--- wpu-portfolio/index.php
+++ wpu-portfolio/index.php
@@ -1,3 +1,28 @@
+<?php
+
+function get_CURL($url)
+{
+  $curl = curl_init();
+  curl_setopt($curl, CURLOPT_URL, $url);
+  curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+  $result = curl_exec($curl);
+  curl_close($curl);
+
+  return $result = json_decode($result, true);
+}
+
+$result = get_CURL('https://www.googleapis.com/youtube/v3/channels?part=snippet,statistics&id=UCkXmLjEr95LVtGuIm3l2dPg&key=AIzaSyC98nbOq8Ra11u6EVXilf3w5Qx_a8KZr9M');
+
+$youtubeProfilePic = $result['items'][0]['snippet']['thumbnails']['medium']['url'];
+$channelName = $result['items'][0]['snippet']['title'];
+$subscriber = $result['items'][0]['statistics']['subscriberCount'];
+
+//latest vidio
+$urlLatestVideo = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyC98nbOq8Ra11u6EVXilf3w5Qx_a8KZr9M&channelId=UCkXmLjEr95LVtGuIm3l2dPg&maxResults=1&order=date&part=snippet';
+
+$result = get_CURL($urlLatestVideo);
+$latestVideoId = $result['items'][0]['id']['videoId'];
+?>
 <!doctype html>
 <html lang="en">
 
@@ -83,14 +108,43 @@
         <div class="col-md-5">
           <div class="row">
             <div class="col-md-4">
-              <img src="img/profile1.png" width="100" class="rounded-circle img-thumbnail">
+              <img src="<?= $youtubeProfilePic; ?>" width="200" class="rounded-circle img-thumbnail">
             </div>
             <div class="col-md-8">
-              <h5>WebProgramingUNPAS</h5>
+              <h5><?= $channelName ?></h5>
+              <p><?= $subscriber ?> Subscribers.</p>
+              <div class="g-ytsubscribe" data-channelid="UCkXmLjEr95LVtGuIm3l2dPg" data-layout="default" data-count="default"></div>
+            </div>
+          </div>
+          <div class="row mt-3 pb-3">
+            <div class="col">
+              <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $latestVideoId ?>?rel=0" allowfullscreen></iframe>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-md-5"></div>
+        <div class="col-md-5">
+          <div class="row">
+            <div class="col-md-4">
+              <img src="img/profile1.png" width="200" class="rounded-circle img-thumbnail">
+            </div>
+            <div class="col-md-8">
+              <h5>@sandhikagalih</h5>
+              <p>7000 Followers.</p>
+            </div>
+          </div>
+
+          <div class="row mt-3 pb-3">
+            <div class="col">
+              <div class="ig-thumbnail">
+                <img src="img/thumbs/1.png" width="100">
+                <img src="img/thumbs/2.png" width="100">
+                <img src="img/thumbs/3.png" width="100">
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -244,6 +298,7 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+  <script src="https://apis.google.com/js/platform.js"></script>
 </body>
 
 </html>
